b96dbbb2d9d6873bbfa27ed5619f7127940f615f
LucasHimelfarb <lucashimelfarb@gmail.com>
2021-08-17 01:18:11 -0300

    main components

---
+++ components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+import NavBar from  './Home/NavBar';
+import DeviceList from './DeviceList';
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <NavBar />
+            <DeviceList />
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
